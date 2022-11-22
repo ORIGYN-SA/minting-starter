c6b0c8d07df6a85e77fe33471e1131a1204ef33f
Motokoder <motokoder@tutanota.com>
2022-11-22 11:16:50 -0700

    SI-202 temp rollback to token prefixes

--- projects/brain-matters-full/deploy.sh
+++ projects/brain-matters-full/deploy.sh
@@ -42,7 +42,7 @@ IDENTITY_NAME="local_deployer"
 COLLECTION_ID="bm"
 DISPLAY_NAME="Brain Matters"
 NAMESPACE="brain.matters"
-TOKEN_WORDS="cerebellum,medulla,brainstem,thalamus,hypothalamus,amygdala,meninges,hippocampus,neocortex,epithalamus,fornix,pons,diencephalon"
+TOKEN_PREFIX="bm-"
 ASSET_MAPPINGS="primary:nft*.png, preview:nft*.png, experience:nft*.html, hidden:mystery-bm.gif"
 SOULBOUND="false"
 
@@ -53,7 +53,7 @@ echo "IDENTITY_NAME: $IDENTITY_NAME"
 echo "COLLECTION_ID: $COLLECTION_ID"
 echo "DISPLAY_NAME: $DISPLAY_NAME"
 echo "NAMESPACE: $NAMESPACE"
-echo "TOKEN_WORDS: $TOKEN_WORDS"
+echo "TOKEN_PREFIX: $TOKEN_PREFIX"
 echo "ASSET_MAPPINGS: $ASSET_MAPPINGS"
 echo "SOULBOUND: $SOULBOUND"
 
@@ -332,9 +332,7 @@ node ./scripts/csm-config.js \
 --collectionDisplayName "$DISPLAY_NAME" \
 --namespace "$NAMESPACE" \
 --collectionId "$COLLECTION_ID" \
---tokenWords "$TOKEN_WORDS" \
---minWords "3" \
---maxWords "3" \
+--tokenPrefix "$TOKEN_PREFIX" \
 --assetMappings "$ASSET_MAPPINGS" \
 --soulbound "$SOULBOUND"
 
